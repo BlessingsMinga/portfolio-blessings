ff059e45b392574650eb370765b4875dc8846ea0
BlessingsMinga <blessingsminga11@gmail.com>
2025-05-14 22:24:22 +0200

    tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover:  '#2a004a',
+        darkTheme: '#11001F',
+      },
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
