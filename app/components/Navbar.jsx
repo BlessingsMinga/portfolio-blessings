11cd0dc5e6b69a9d70937ac7026a6d18e0ae7420
BlessingsMinga <blessingsminga11@gmail.com>
2025-05-21 15:09:35 +0200

    added a blur drop on the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,72 +5,71 @@ import React, { useRef } from 'react'
 const Navbar = () => {
 
     const sideMenuRef = useRef();
-    const openMenu = ()=>{
+    const openMenu = () => {
         sideMenuRef.current.style.transform = 'translateX(-16rem)'
     }
-    const closeMenu = ()=>{
+    const closeMenu = () => {
         sideMenuRef.current.style.transform = 'translateX(16rem)'
     }
 
-  return (
-    <>
-    <div className='fixed top-10 right-0 w-11/12 -z-10 translate-y-[-70%]'>
-        <Image src={assets.Blob} alt='' className='w-full'/>
+    return (
+        <>
+            <div className='fixed top-10 right-0 w-11/12 -z-10 translate-y-[-70%]'>
+                <Image src={assets.Blob} alt='' className='w-full' />
 
-    </div>
+            </div>
 
 
-    <nav className='w-full fixed px-5 lg:px-8 xl:px-[-8%] py-4 
+            <nav className='w-full fixed px-5 lg:px-8 xl:px-[-8%] py-4 
     flex items-center justify-between z-50'>
-        <a href="#top">
-            <Image src={assets.BlessingsM_} className='w-35 cursor-pointer mr-9' alt="Blessings Minga Logo"/>
-        </a>
+                <a href="#top">
+                    <Image src={assets.BlessingsM_} className='w-35 cursor-pointer mr-9' alt="Blessings Minga Logo" />
+                </a>
 
-        <ul className='hidden md:flex items-center gap-7 lg:gap-13
-        rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
-            <li><a href="#top">Home</a></li>
-            <li><a href="#about">About me</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#work">My Work</a></li>
-            <li><a href="#contact">Contact me</a></li>
-        </ul>
+                <ul className='hidden md:flex items-center gap-7 lg:gap-13
+                    rounded-full px-12 py-3 bg-white/50 shadow-sm backdrop-blur-sm'>
+                    <li><a href="#top">Home</a></li>
+                    <li><a href="#about">About me</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#work">My Work</a></li>
+                    <li><a href="#contact">Contact me</a></li>
+                </ul>
+                <div className='flex items-center gap-4'>
 
-        <div className='flex items-center gap-4'>
+                    <button>
+                        <Image src={assets.moon_icon} alt='' className='w-6' />
+                    </button>
+                    <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5
+            border border-gray-500 rounded-full ml-4'> Contact
+                        <Image src={assets.arrow_icon} className='w-3' alt="Contact Us" />
+                    </a>
 
-            <button>
-                <Image src={assets.moon_icon} alt='' className='w-6' />
-            </button>
-            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5
-            border border-gray-500 rounded-full ml-4'> Contact 
-                <Image src={assets.arrow_icon} className='w-3' alt="Contact Us" />
-            </a>
+                    <button className='block md:hidden ml-3' onClick={openMenu}>
+                        <Image src={assets.menu_black} alt='' className='w-6' />
+                    </button>
+                </div>
 
-            <button className='block md:hidden ml-3' onClick={openMenu}>
-            <Image src={assets.menu_black} alt='' className='w-6'/>
-            </button>
-        </div>
+                { /* ----- ----- mobile menu ----- ----- */}
 
-        { /* ----- ----- mobile menu ----- ----- */ }
-
-        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 
+                <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 
         w-64 z-50 h-screen bg-rose-50 transition duration-500'>
 
-            <div onClick={closeMenu} className='absolute right-6 top-6'>
-                <Image src={assets.close_black} alt='' className='w-5 cursor-pointer' />
-            </div>
-            <li><a onClick={closeMenu} href="#top">Home</a></li>
-            <li><a onClick={closeMenu} href="#top">About me</a></li>
-            <li><a onClick={closeMenu} href="#services">Services</a></li>
-            <li><a onClick={closeMenu} href="#work">My Work</a></li>
-            <li><a onClick={closeMenu} href="#contact">Contact me</a></li>
-        </ul>
-    </nav>
+                    <div onClick={closeMenu} className='absolute right-6 top-6'>
+                        <Image src={assets.close_black} alt='' className='w-5 cursor-pointer' />
+                    </div>
+                    <li><a onClick={closeMenu} href="#top">Home</a></li>
+                    <li><a onClick={closeMenu} href="#top">About me</a></li>
+                    <li><a onClick={closeMenu} href="#services">Services</a></li>
+                    <li><a onClick={closeMenu} href="#work">My Work</a></li>
+                    <li><a onClick={closeMenu} href="#contact">Contact me</a></li>
+                </ul>
+            </nav>
 
 
 
-    </>
-      
-  )
+        </>
+
+    )
 }
 
 export default Navbar
