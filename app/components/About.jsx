bb2d4a574326e98af6c9c878e975f6adba8e98a1
BlessingsMinga <blessingsminga11@gmail.com>
2025-05-21 15:20:18 +0200

    added a box profile to the web page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,12 +1,21 @@
+import Image from 'next/image'; 
+import { assets } from '../../assets/assets';
 import React from 'react'
 
 const About = () => {
   return (
     <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
 
-        <h4 className='text-center mb-2 text-lg font-robotMono'>Introduction</h4>
-        <h2>About me</h2>
-      
+        <h4 className='text-center mb-2 text-lg'>Introduction</h4>
+        <h2 className='text-center text-5xl'>About me</h2>
+
+        <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+          <div>
+            <Image src={assets.box_profile} alt='Blessings Minga Image' className='w-full rounded-3xl'/>
+          </div>
+
+          <div className='flex-1'></div>
+        </div>
     </div>
   )
 }
