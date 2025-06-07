42fe89d4398eef2392d81d86c353fde027271c7d
BlessingsMinga <blessingsminga11@gmail.com>
2025-06-07 06:19:08 +0200

    google verification

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <meta name="google-site-verification" content="kbUndA4XesT6Vmn2AhkhFjsDaKmyMPinKvW5KVRo2TI" />
       <body
         className={`font-sans leading-8 
         overflow-x-hidden`}
