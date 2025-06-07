be4288fbc0508a652d1ead4378629f638f2adef6
BlessingsMinga <blessingsminga11@gmail.com>
2025-06-07 07:08:36 +0200

    Made some changes to improve SEO

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 
 
 export const metadata = {
-  title: "Welcome to my portfolio",
+  title: "Blessings Minga | Full Stack Designer & Developer in Malawi ",
   description: "",
 };
 
@@ -11,6 +11,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <meta name="google-site-verification" content="kbUndA4XesT6Vmn2AhkhFjsDaKmyMPinKvW5KVRo2TI" />
+      <meta name="description" content="I'm a Malawi-based web developer and designer. I also work on graphics, UX/UI,
+          branding, and data. I love using tech and creativity to solve problems and build cool things."></meta>
+      <meta name="description" content="Software developer in Malawi, Hire a web developer in Lilongwe, Malawi tech freelancer, IT services Malawi"></meta>
       <body
         className={`font-sans leading-8 
         overflow-x-hidden`}
